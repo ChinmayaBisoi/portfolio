675430693b7f0a3a35e313e65707c187274c3cfc
C_bi__ <chinmaya@kloudle.com>
2023-02-07 00:58:06 +0530

    added about componetn

---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const About = (props: Props) => {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h2>About</h2>
+    </div>
+  );
+};
+
+export default About;
