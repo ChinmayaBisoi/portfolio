605f7aa150fa1dd9b117ce1106a3466f032f4aa2
C_bi__ <chinmaya@kloudle.com>
2023-02-07 00:58:20 +0530

    header component made sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,35 +6,39 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header className="text-gray-400 px-4 sticky top-0 z-50 max-w-5xl mx-auto flex items-center justify-between">
-      <motion.div
-        initial={{ x: -300, opacity: 0, scale: 0.5 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ duration: 1.2 }}>
-        <SocialIcon
-          url="https://twitter.com/C_bi__"
-          bgColor="transparent"
-          fgColor="gray"
-        />
-        <SocialIcon
-          url="https://github.com/ChinmayaBisoi"
-          bgColor="transparent"
-          fgColor="gray"
-        />
-      </motion.div>
-      <motion.div
-        initial={{ x: 300, opacity: 0, scale: 0.5 }}
-        animate={{ x: 0, opacity: 1, scale: 1 }}
-        transition={{ duration: 1.2 }}
-        className="flex items-center cursor-pointer hover:underline underline-offset-4">
-        <SocialIcon
-          className="cursor-pointer"
-          network="email"
-          bgColor="transparent"
-          fgColor="gray"
-        />
-        <p className="uppercase hidden md:inline-block text-sm">Get In Touch</p>
-      </motion.div>
+    <header className="sticky top-0 bg-bg-primary shadow-md text-gray-400 px-4 z-50 max-w-5xl mx-auto">
+      <nav className="flex items-center justify-between">
+        <motion.div
+          initial={{ x: -300, opacity: 0, scale: 0.5 }}
+          animate={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ duration: 1.2 }}>
+          <SocialIcon
+            url="https://twitter.com/C_bi__"
+            bgColor="transparent"
+            fgColor="gray"
+          />
+          <SocialIcon
+            url="https://github.com/ChinmayaBisoi"
+            bgColor="transparent"
+            fgColor="gray"
+          />
+        </motion.div>
+        <motion.div
+          initial={{ x: 300, opacity: 0, scale: 0.5 }}
+          animate={{ x: 0, opacity: 1, scale: 1 }}
+          transition={{ duration: 1.2 }}
+          className="flex items-center cursor-pointer hover:underline underline-offset-4">
+          <SocialIcon
+            className="cursor-pointer"
+            network="email"
+            bgColor="transparent"
+            fgColor="gray"
+          />
+          <p className="uppercase hidden md:inline-block text-sm">
+            Get In Touch
+          </p>
+        </motion.div>
+      </nav>
     </header>
   );
 };
